670af11836238f83ae774882864ef995bd2254f0
Alexander <Prichman@users.noreply.github.com>
2014-07-10 20:25:24 +0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include "MainWindow.h"
+
+#include <QtWidgets/QApplication>
+
+
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+
+	MainWindow window;
+	window.show();
+
+	return a.exec();
+}
